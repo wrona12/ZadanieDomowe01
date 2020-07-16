fc9d3fa49142f2d6f8334f82b970037b6c5a5790
Wrona <bartlomiejgawron@wp.pl>
2020-07-16 21:45:43 +0200

    Zadanie domowe

---
+++ src/pl/zadanie/domowe01/Main.java
@@ -0,0 +1,11 @@
+package pl.zadanie.domowe01;
+
+public class Main {
+    public static void main(String[] args) {
+        addSomething();
+    }
+
+    private static void addSomething() {
+        System.out.println("Added something");
+    }
+}
